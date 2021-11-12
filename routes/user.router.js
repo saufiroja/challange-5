10f7464020e8f33314e2abddc3dde32447100fa3
saufiroja <saufiroja08@gmail.com>
2021-11-12 16:07:19 +0800

    membuat error handling

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 
 const { register, login } = require("../controllers/user.controller");
+const { validate } = require("../middlewares/validate.middleware");
+const { registerSchema } = require("../schema/register.schema");
 
-router.post("/register", register);
+router.post("/register", validate(registerSchema), register);
 router.post("/login", login);
 
 module.exports = router;
