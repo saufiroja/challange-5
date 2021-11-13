6dca532827cca41d15b1cee8fbe0b2d71a19400e
saufiroja <saufiroja08@gmail.com>
2021-11-13 22:28:38 +0800

    konfigurasi ejs

--- app.js
+++ app.js
@@ -2,26 +2,34 @@ require("dotenv").config();
 
 const express = require("express");
 const userRouter = require("./routes/user.router");
+const dashboardRouter = require("./routes/dashboard.controller");
+const errorHandler = require("./error/Error.Handler");
 const app = express();
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// konfigurasi ejs
+app.set("view engine", "ejs");
+
 // router
 app.use(userRouter);
+app.use(dashboardRouter);
 
-// error handling
-app.use((err, req, res, next) => {
-  console.log(err);
-  const { message, code = 500, error = "internal server error" } = err;
+// error handler
+app.use(errorHandler);
+// // error handling
+// app.use((err, req, res, next) => {
+//   console.log(err);
+//   const { message, code = 500, error = "internal server error" } = err;
 
-  return res.status(code).json({
-    message,
-    code,
-    error,
-  });
-});
+//   return res.status(code).json({
+//     message,
+//     code,
+//     error,
+//   });
+// });
 
 // listen
 const PORT = process.env.PORT;
