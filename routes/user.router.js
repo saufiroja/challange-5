5b97c5eb1a11562ff3f8f7d576606391c3f8bf18
saufiroja <saufiroja08@gmail.com>
2021-11-13 22:30:02 +0800

    menambah validasi login

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,10 +1,21 @@
 const router = require("express").Router();
 
-const { register, login } = require("../controllers/user.controller");
+const {
+  register,
+  login,
+  viewLogin,
+  viewRegister,
+} = require("../controllers/user.controller");
 const { validate } = require("../middlewares/validate.middleware");
 const { registerSchema } = require("../schema/register.schema");
+const { loginSchema } = require("../schema/login.schema");
 
+// GET
+router.get("/register", viewRegister);
+router.get("/login", viewLogin);
+
+// POST
 router.post("/register", validate(registerSchema), register);
-router.post("/login", login);
+router.post("/login", validate(loginSchema), login);
 
 module.exports = router;
