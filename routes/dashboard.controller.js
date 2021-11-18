c630995e701370b70d2a08ae6d6378a37101bbc2
saufiroja <saufiroja08@gmail.com>
2021-11-18 15:10:07 +0800

    delete

--- routes/dashboard.controller.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-const { dashboard } = require("../controllers/dashboard.controller");
-
-router.get("/dashboard", dashboard);
-
-module.exports = router;
